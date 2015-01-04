71c1cae6ec4897036514dee2a1d4345b5090c2af
Nelson Elhage <nelhage@nelhage.com>
2015-01-04 14:52:04 +0100

    reptyr 0.6

--- reptyr.h
+++ reptyr.h
@@ -20,7 +20,7 @@
  * THE SOFTWARE.
  */
 
-#define REPTYR_VERSION "0.5dev"
+#define REPTYR_VERSION "0.6"
 
 #define assert_nonzero(expr) ({                         \
             typeof(expr) __val = expr;                  \
