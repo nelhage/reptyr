8a390e46ba10774ede1e16c28ffe7aca9259d0fa
Ville Skyttä <ville.skytta@iki.fi>
2015-11-05 23:16:32 +0200

    Make tty-steal test work with Python 3

--- test/tty-steal.py
+++ test/tty-steal.py
@@ -4,7 +4,7 @@ import os
 import sys
 
 if os.getenv("NO_TEST_STEAL") is not None:
-    print "Skipping tty-stealing tests because $NO_TEST_STEAL is set."
+    print("Skipping tty-stealing tests because $NO_TEST_STEAL is set.")
     sys.exit(0)
 
 PR_SET_PTRACER_ANY = 0xffffffff
